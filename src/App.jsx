19f03f7dbbe8e52d661559424d0329dbd95fee19
Naman Sharma <namancomtech@gmail.com>
2026-04-07 10:09:47 +0530

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ShoppingList from "./components/ShoppingList";
 import ShoppingCart from "./components/ShoppingCart";
 import Details from "./components/Details";
@@ -6,13 +6,13 @@ import Details from "./components/Details";
 function App() {
     return (
         <div>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<ShoppingList />} />
                     <Route path="/mycart" element={<ShoppingCart />} />
                      <Route path='/details/:id' element={<Details/>}></Route>  {/*dynamic URL */}
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </div>
     );
 }
